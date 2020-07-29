185d22691cc7f4bc2a6b0ca487983225a96c78b2
Adewole Joel <joeladewole3@gmail.com>
2020-07-29 23:11:32 +0100

    Added Site Favicon

--- mimeal/index.php
+++ mimeal/index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/style.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="shortcut icon" href="./assets/images/logo.png" type="image/x-icon">
 </head>
 <body>
     <nav>
